4bfdc8f5aef153cab4950979ecfd0474c37dd4c4
HY-ChoiSanghyun <sanghyun.choi@hynux.com>
2026-04-07 12:29:18 +0900

    Fix Prisma 7 build error by removing deprecated datasources config in PrismaClient

--- lib/db.ts
+++ lib/db.ts
@@ -4,13 +4,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function getClient(): PrismaClient {
   if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-    })
+    globalForPrisma.prisma = new PrismaClient()
   }
   return globalForPrisma.prisma
 }
